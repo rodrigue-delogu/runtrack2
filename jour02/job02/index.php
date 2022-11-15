63779d743a352cda503da5fe2eeb69f3a72e89c0
rodrigue-delogu <114388360+rodrigue-delogu@users.noreply.github.com>
2022-11-15 15:47:38 +0100

    job 2 fini

---
+++ jour02/job02/index.php
@@ -0,0 +1,21 @@
+<?php
+$x = 0;
+while($x <= 1337){
+    if ($x == 26){
+        echo "";
+    } 
+    if ($x == 37){
+        echo "";
+    } 
+    if ($x == 88){
+        echo "";
+    } 
+    if ($x == 1111){
+        echo "";
+    } 
+    else {
+        echo "$x <br>";
+    }
+        $x++;
+}
+?>
