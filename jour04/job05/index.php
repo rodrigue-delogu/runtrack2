51526c4b6161d0058d153dc5b7fb5dbfda7823e9
rodrigue-delogu <114388360+rodrigue-delogu@users.noreply.github.com>
2022-11-17 15:12:10 +0100

    job06

--- jour04/job05/index.php
+++ jour04/job05/index.php
@@ -8,20 +8,22 @@
 </head>
 <body>
 <form action="" method="post" class="formCol">
-        <label for="nom">Quel est votre username: <br></label>
-        <input type="text" name="username" id="nom"><br>
+        <label for="username">Quel est votre username: <br></label>
+        <input type="username" name="username" id="username"><br>
 
-        <label for="prenom">Quel est votre password: <br></label>
-        <input type="password" name="password" id="prenom"><br>
+        <label for="password">Quel est votre password: <br></label>
+        <input type="password" name="password" id="password"><br>
 
         <input type="submit" value="Envoyer le formulaire">
     </form>
+
 <?php
+    if (isset($_POST["username"]) && isset($_POST["password"])){
         if ($_POST["username"] == "John" && $_POST["password"] == "Rambo"){
             echo "C'est pas ma guerre";
         }
         else{
         echo "<i>Votre pire cauchemar</i>";
         }
-      
+} 
 ?>
