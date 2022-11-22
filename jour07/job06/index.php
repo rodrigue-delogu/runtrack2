1d7acba938e2792d2a9a1b6dbd58b5f26d8e419d
rodrigue-delogu <114388360+rodrigue-delogu@users.noreply.github.com>
2022-11-22 13:53:52 +0100

    job06 done

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -1,7 +1,31 @@
 <?php 
 
-$str = "ABEGLST"
-    leetSpeak($str){
-        if("A")
+$str = "ABEGLST";
+
+function leetSpeak($str){
+    for ($i=0; isset($str[$i]); $i++){
+        if("A" === $str[$i]){
+            echo 4;        
+        }
+        elseif("B" === $str[$i]){
+            echo 8;        
+        }
+        elseif("E" === $str[$i]){
+            echo 3;        
+        }
+        elseif("G" === $str[$i]){
+            echo 6;        
+        }
+        elseif("L" === $str[$i]){
+            echo 1;        
+        }
+        elseif("S" === $str[$i]){
+            echo 5;        
+        }
+        elseif("T" === $str[$i]){
+            echo 7;        
+        }
     }
+}
+echo leetSpeak($str);
 ?>
