cddf28e99e71437fbc2b1d956584ad2ce5c19a29
rodrigue-delogu <114388360+rodrigue-delogu@users.noreply.github.com>
2022-11-22 10:26:06 +0100

    job02 done

---
+++ jour07/job02/index.php
@@ -0,0 +1,14 @@
+<?php
+$arg = TRUE;
+$arg1 = FALSE;
+function bonjour($jour){
+    if ($jour == true){
+        echo "Bonjour";
+    }
+    if ($jour == false){
+        echo "Bonsoir";
+    }
+}
+
+bonjour($arg);
+bonjour($arg1);
